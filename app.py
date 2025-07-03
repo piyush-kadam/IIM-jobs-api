55625822a3da828d8840bab8c94638864cdfed67
piyush-kadam <piyushkadam3333@gmail.com>
2025-07-03 09:57:40 +0530

    Remove webdriver-manager and switch to local chromedriver

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
 from flask import Flask, request, jsonify
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
 import time
 import uuid
 import re
+import os
 
 app = Flask(__name__)
 sessions = {}
-
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-import os
 
 def create_driver():
     chrome_options = Options()
